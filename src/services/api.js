fd434ea34a309c270ec0df98ee0512e179fd3597
doravantebeto <roberto.araujo339@gmail.com>
2021-03-22 01:51:29 -0300

    Updated production API

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3333'
-    : 'https://apipye.betocosta.com.br/';
+    : 'https://api-pick-your-error.herokuapp.com/';
 
 const api = axios.create({
   baseURL,
